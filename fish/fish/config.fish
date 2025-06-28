dbdd466bb8c0a6dc9f7f77a3a91a96db29e2c51d
Cletus <osternaud.clement@gmail.com>
2025-06-28 14:41:58 +0200

    feat: Refactor ssh-agent initialization and key loading in fish config

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -6,3 +6,13 @@ alias dev="cd ~/dev"
 if status is-interactive
   # Commands to run in in interactive mode
 end
+
+# Start ssh-agent automatically
+if not pgrep -u (whoami) ssh-agent > /dev/null
+    eval (ssh-agent -c)
+end
+
+# Load all keys
+for key in ~/.ssh/id_*
+    ssh-add $key >/dev/null ^&1
+end
